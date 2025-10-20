b6ada26786a999394d00f595b5ca4244bee521c4
Jonathan <jgor.depredador@gmail.com>
2025-10-19 20:27:31 -0600

    Desactivé conexión a MySQL temporalmente para despliegue inicial

--- index.js
+++ index.js
@@ -13,12 +13,12 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
-const connection = mysql.createConnection({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME
-});
+// const connection = mysql.createConnection({
+//   host: process.env.DB_HOST,
+//   user: process.env.DB_USER,
+//   password: process.env.DB_PASSWORD,
+//   database: process.env.DB_NAME
+// });
 
 app.get('/', (req, res) => {
     res.render('index');
